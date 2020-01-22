288891c4de823c59f52b0389f738c94447b6a43c
Shivam Agrawal <shivam301296@gmail.com>
2020-01-22 23:56:36 +0530

    updated

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,36 @@
+import "./SeasonDisplay.css";
+import React from "react";
+
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Buu, It's cold",
+    iconName: "snowflake"
+  }
+};
+
+const getSeason = (lat, month) => {
+  if (month > 2 && month < 9) {
+    return lat > 0 ? "summer" : "winter";
+  } else {
+    return lat < 0 ? "summer" : "winter";
+  }
+};
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth());
+  const { text, iconName } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left icon massive ${iconName}`} />
+      <h1>{text}</h1>
+      <i className={`icon-right icon massive ${iconName}`} />
+    </div>
+  );
+};
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
+class App extends React.Component {
+  state = {
+    lat: null,
+    lnt: null
+  };
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state= { lat: null };
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     lat: null,
+  //     lnt: null
+  //   };
+  // }
 
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                this.setState({ lat: position.coords.latitude});
-            },
-            err => console.log(err)
-        );
+  componentDidMount() {
+    console.log("Component rendered on screen");
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+          errMsg: ""
+        });
+      },
+      err => this.setState({ errMsg: err.message })
+    );
+  }
+
+  componentDidUpdate() {
+    console.log("Component updated");
+  }
+
+  componentWillUnmount() {
+    console.log("Component is going to unmount");
+  }
+
+  renderContent() {
+    if (this.state.lat && !this.state.errMsg) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else if (!this.state.lat && this.state.errMsg) {
+      return <div>Error: {this.state.errMsg}</div>;
     }
+    return <Spinner text="Please accept location request" />;
+  }
 
-    render(){
-        return <div>Latitute: {this.state.lat}</div>
-    }
+  render() {
+    console.log("Rendering Component");
+    return (
+      <div style={{ border: "10px solid green" }}>{this.renderContent()}</div>
+    );
+  }
 }
 
-
-ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector("#root"));
