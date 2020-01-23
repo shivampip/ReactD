a8803ba785cbc69c592628f0ec9bbab2e3c2c1a4
Shivam Agrawal <shivam301296@gmail.com>
2020-01-24 00:55:52 +0530

    updated

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,27 +1,30 @@
-import './App.css';
-import React from 'react'
+import "./App.css";
+import React from "react";
 // import axios from 'axios';
-import SearchBar from './SearchBar';
-import unsplash from '../api/unsplash';
+import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
+import unsplash from "../api/unsplash";
 
-class App extends React.Component{
-    state= { images: []};
+class App extends React.Component {
+  state = { images: [] };
 
-    onTermSubmit= async (term)=> {
-        console.log("Search: "+term);
-        const response= await unsplash.get("/search/photos",{
-            params:{query: term} 
-        })
-        console.log(response.data.results);
-        this.setState({images: response.data.results});
-    }
+  onTermSubmit = async term => {
+    console.log("Search: " + term);
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
+    });
+    console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
 
-    render(){
-        return <div>
-            <SearchBar onTermSubmit= {this.onTermSubmit}/>
-            Found: {this.state.images.length} images.
-            </div>
-    }
+  render() {
+    return (
+      <div>
+        <SearchBar onTermSubmit={this.onTermSubmit} />
+        <ImageList images={this.state.images} />
+      </div>
+    );
+  }
 }
 
 export default App;
