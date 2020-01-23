3ae9b5164d11bf5fde3d1241addaa943fe38b3af
shivampip <shivam301296@gmail.com>
2020-01-23 16:09:49 +0530

    updated

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -7,20 +7,6 @@ import unsplash from '../api/unsplash';
 class App extends React.Component{
     state= { images: []};
 
-    // onTermSubmit(term){
-    //     console.log("Search: "+term);
-    //     axios.get("https://api.unsplash.com/search/photos",{
-    //         headers: {
-    //             Authorization: 'Client-ID a7201dc2e0f593d5afec59f5df5b8b5bd61c860877042f34d21078fade0469c8'
-    //         },
-    //         params:{
-    //             query: term
-    //         }
-    //     }).then((response)=>{
-    //         console.log(response.data.results);
-    //     });
-    // }
-
     onTermSubmit= async (term)=> {
         console.log("Search: "+term);
         const response= await unsplash.get("/search/photos",{
